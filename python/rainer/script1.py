9939d471d05330f4830d16eefdd278b6d292364b
DOOM <rainerastodillo27@gmail.com>
2026-02-18 13:50:37 +0800

    Add files via upload

---
+++ python/rainer/script1.py
@@ -0,0 +1,16 @@
+from script2 import *
+def favorite_games(game):
+    print(f"My favorite games is {game}")
+
+def main():
+    print("This is the script.")
+    favorite_games("Wuthering Waves")
+    favorite_games("Genshin Impact")
+    favorite_games("Honkai star rail")
+
+    print("All of this is my favorite games")
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/rainer/script2.py
@@ -0,0 +1,14 @@
+
+from script1 import *
+
+def favorite_food(food):
+    print(f"My favorite food is {food}")
+
+def main():
+    print("this is the script 2")
+    favorite_games("Wuthering Waves")
+    favorite_food("Pizza")
+    print("adios")
+
+if __name__ == '__main__':
+    main()
