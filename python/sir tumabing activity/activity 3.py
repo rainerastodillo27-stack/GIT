cacda9844ad6ea1280ec7e305c8eb2d0456e31fa
Rainer Astodillo <rainerastodillo27@gmail.com>
2026-03-05 10:33:20 +0800

    deleted the try

--- python/sir tumabing activity/activity 3.py
+++ python/sir tumabing activity/activity 3.py
@@ -22,45 +22,45 @@ first_name = input("First Name: ")
 
 print("\nEnter Midterm Grades")
 
-try:
-    # -------- Midterm grade --------
-    quiz = float(input("Quiz/Activity (40%): "))
-    project = float(input("Project (30%): "))
-    exam = float(input("Exam (20%): "))
-    participation = float(input("Class Participation (10%): "))
+# -------- Midterm grade --------
+quiz = float(input("Quiz/Activity (40%): "))
+project = float(input("Project (30%): "))
+exam = float(input("Exam (20%): "))
+participation = float(input("Class Participation (10%): "))
 
-    midterm = (quiz * 0.40) + (project * 0.30) + (exam * 0.20) + (participation * 0.10)
-    print("Midterm Grade:", (midterm))
+midterm = (quiz * 0.40) + (project * 0.30) + (exam * 0.20) + (participation * 0.10)
+print("Midterm Grade:", (midterm))
 
-    # -------- Endterm grade --------
-    print("\nEnter Endterm Grades")
+# -------- Endterm grade --------
+print("\nEnter Endterm Grades")
 
-    quiz2 = float(input("Quiz/Activity (40%): "))
-    project2 = float(input("Project (30%): "))
-    exam2 = float(input("Exam (20%): "))
-    participation2 = float(input("Class Participation (10%): "))
+quiz2 = float(input("Quiz/Activity (40%): "))
+project2 = float(input("Project (30%): "))
+exam2 = float(input("Exam (20%): "))
+participation2 = float(input("Class Participation (10%): "))
 
-    endterm = (quiz2 * 0.40) + (project2 * 0.30) + (exam2 * 0.20) + (participation2 * 0.10)
-    print("Endterm Grade:", (endterm))
+endterm = (quiz2 * 0.40) + (project2 * 0.30) + (exam2 * 0.20) + (participation2 * 0.10)
+print("Endterm Grade:", (endterm))
 
-    # -------- FINAL GRADE --------
-    final_grade = (midterm * 0.40) + (endterm * 0.60)
 
-    print("\n=== FINAL RESULT ===")
-    print("Student:", first_name, last_name)
-    print("Final Grade:", (final_grade))
+# -------- Final grade  --------
+final_grade = (midterm * 0.40) + (endterm * 0.60)
 
-    # -------- Ang imong Remarks --------
-    if final_grade >= 90:
+print("\n=== FINAL RESULT ===")
+print("Student:", first_name, last_name)
+print("Final Grade:", (final_grade))
+
+
+ # -------- Ang Remarks --------
+if final_grade >= 90:
         print("Remarks: A")
-    elif final_grade >= 80:
+elif final_grade >= 80:
         print("Remarks: B")
-    elif final_grade >= 70:
+elif final_grade >= 70:
         print("Remarks: C")
-    elif final_grade >= 60:
+elif final_grade >= 60:
         print("Remarks: D")
-    else:
-        print("Remarks: F")
+else:
+    print("Remarks: F")
 
-except ValueError:
     print("Invalid input! Please enter numbers only.")
